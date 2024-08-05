ebaa17a890dbbfc986ced275c238fe82d492ca57
todor02 <todort02@abv.bg>
2024-08-05 15:39:20 +0300

    implemented frontend form for CV extraction and used the model to rate it

---
+++ Forms/view_cv.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from functions import *
+
+app = ctk.CTk()
+init(app, "Check CV")
+
+
+
+app.mainloop()
--- hiring_service/upload_file.py
+++ hiring_service/upload_file.py
@@ -9,36 +9,27 @@ def upload_file():
         title="Select a File",
         filetypes=(("PDF files", "*.pdf"), ("all files", "*.*"))
     )
-    
-    # Check if a file was selected
-    if file_path:
-        # Get the directory where you want to save the file
-        upload_directory = os.path.join(os.getcwd(), 'uploads')
-        
-        # Create the directory if it doesn't exist
-        if not os.path.exists(upload_directory):
-            os.makedirs(upload_directory)
-        
-        # Get the base name of the file to keep the original file name
-        file_name = os.path.basename(file_path)
-        
-        # Define the path where the file will be copied
-        destination = os.path.join(upload_directory, file_name)
-        
-        # Copy the file to the destination
-        shutil.copy(file_path, destination)
-        
-        print(f'File uploaded to {destination}')
-    else:
-        print('No file selected')
+    try:
+        # Check if a file was selected
+        if file_path:
+            # Get the directory where you want to save the file
+            upload_directory = os.path.join(os.getcwd(), '../hiring_service/uploads')
 
-# Create the main application window
-root = tk.Tk()
-root.title("File Upload")
+            # Create the directory if it doesn't exist
+            if not os.path.exists(upload_directory):
+                os.makedirs(upload_directory)
 
-# Create a button that calls the upload_file function when clicked
-upload_button = tk.Button(root, text="Upload CV", command=upload_file)
-upload_button.pack(pady=20)
+            # Get the base name of the file to keep the original file name
+            file_name = os.path.basename(file_path)
 
-# Start the GUI event loop
-root.mainloop()
+            # Define the path where the file will be copied
+            destination = os.path.join(upload_directory, file_name)
+
+            # Copy the file to the destination
+            shutil.copy(file_path, destination)
+
+            print(f'File uploaded to {destination}')
+        else:
+            print('No file selected')
+    except Exception as e:
+        print(f'Error: {e}')
